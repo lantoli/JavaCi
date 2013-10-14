71e9779fa855661c97dbdd9abd3240658d80ad12
Leo Antoli <leo.antoli@gmail.com>
2013-10-14 15:54:52 +0200

    initial commit

---
+++ src/main/java/javaci/JavaCiBitbucket/App.java
@@ -0,0 +1,13 @@
+package javaci.JavaCiBitbucket;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
